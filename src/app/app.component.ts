abc2fd46a8f71aefd39b352fb7947393274b5b37
SHAYCRITICAL\shayr <shayramos@hotmail.com>
2024-02-02 15:55:27 +0000

    Validate NIF

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormFeatureComponent } from './modules/form-feature/form-feature.component';
-import { HttpClientModule } from '@angular/common/http';
-import { NifValidatorService } from './core/services/nif-validator.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormFeatureComponent, HttpClientModule],
-  providers: [NifValidatorService],
+  imports: [RouterOutlet, FormFeatureComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
--- src/app/modules/form-feature/form-feature.component.ts
+++ src/app/modules/form-feature/form-feature.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
-import { NifValidatorService } from '../../core/services/nif-validator.service';
 import { CustomDropdownComponent, DropdownType } from '../../shared/components/custom-dropdown/custom-dropdown.component';
 
 @Component({
@@ -16,17 +15,10 @@ export class FormFeatureComponent {
     name: new FormControl('', Validators.required),
     email: new FormControl('', [Validators.required, Validators.email]),
     birthdate: new FormControl('', [Validators.required, this.dateValidator()]),
-    phone: new FormControl('', [Validators.required, Validators.pattern(/^-?(0|[1-9]\d*)?$/)]), // TODO: Permitir digitar apenas números
-    taxnumber: new FormControl('', Validators.required), // TODO: Validar
+    phone: new FormControl('', [Validators.required, Validators.pattern(/^-?(0|[1-9]\d*)?$/)]),
+    taxnumber: new FormControl('', [Validators.required, this.NIFValidator()]), // TODO: Permitir digitar apenas números
     country: new FormControl('', Validators.required),
   })
-
-  // TODO: The "Tax number" field only accepts valid portuguese Individual Identification Number (Número de Identificação Fiscal (NIF) )
-  // TODO: Erro no console
-
-  // constructor(private nifValidatorService: NifValidatorService) { 
-  //   this.formGroup?.valueChanges?.subscribe(console?.info);
-  // }
 
   protected options = [
     { code: '', name: '' },
@@ -36,27 +28,55 @@ export class FormFeatureComponent {
   ] as DropdownType[];
 
   onSubmit(): void {
-    // TODO: Unsubiscribe
-    // this.nifValidatorService.getNIF('320121704').subscribe((response: any) => {
-    //   console.log(response);
-    // })
-    console.log(this.formGroup)
     if (this.formGroup.valid) {
       window.location.reload()
+    }
+  }
+
+  private NIFValid(nif: string): boolean {
+    if (nif?.length !== 9)
+    return false
+    
+    const controlDigt = Number(nif.at(8))
+    const arrNif = nif.split('')
+    let sum = 0
+    let countControl = 9
+    for (let index = 0; index < arrNif.length-1; index++) {
+      sum += Number(arrNif[index])*countControl;
+      countControl -= 1
+    }
+    
+    const sumModulo = sum % 11
+    const sumComplement = 11 - sumModulo
+    const result = sumComplement > 9 ? 0 : sumComplement
+    
+    return (result === controlDigt)
+  }
+
+  private NIFValidator(): ValidatorFn {
+    return (control: AbstractControl): {[key: string]: any} | null => {
+      if (!(control && control?.value)) {
+        return null;
+      }
+    
+      return !this.NIFValid(control?.value) 
+        ? { invalidNIF: 'NIF invalid' } 
+        : null;
     }
   }
 
   private dateValidator(): ValidatorFn {
     return (control: AbstractControl): {[key: string]: any} | null => {
       const today = new Date().getTime();
-  
+      
       if (!(control && control?.value)) {
         return null;
       }
-  
+    
       return new Date(control?.value).getTime() > today 
         ? { invalidDate: 'Future date invalid' } 
         : null;
     }
   }
+
 }
